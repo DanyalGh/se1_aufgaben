3c2279384a50aa68a985dd20249cd3a819167a87
Jeremy Nieth <jnieth@live.de>
2020-05-02 18:37:46 +0200

    Update Sort.java

--- A2.2/src/Sort.java
+++ A2.2/src/Sort.java
@@ -4,16 +4,25 @@ public class Sort implements SortIF {
     
     @Override
     public void readDependencies(String[][] dependencies) {
-
         this.dependencies = dependencies;
     }
 
     @Override
     public boolean isWellSorted(String[] sequence) {
-
-        for (String task: sequence) {
+        int i = 0;
+        for (String task : sequence) {
+            for (String[] dependency : dependencies) {
+                if (dependency[1].equals(task)) {
+                    for (int j = i; j < sequence.length; j++) {
+                        if (dependency[0].equals(sequence[j])) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            i++;
         }
     
-        return false;
+        return true;
     }
 }
