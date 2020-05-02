54f53902abe89d588177573f83c1c4f730ba3e6e
DanyalGh <60552630+DanyalGh@users.noreply.github.com>
2020-05-02 18:20:04 +0200

    add methods

--- A2.2/src/Sort.java
+++ A2.2/src/Sort.java
@@ -1,11 +1,19 @@
 public class Sort implements SortIF {
+    
+    String[][] dependencies;
+    
     @Override
     public void readDependencies(String[][] dependencies) {
 
+        this.dependencies = dependencies;
     }
 
     @Override
     public boolean isWellSorted(String[] sequence) {
+
+        for (String task: sequence) {
+        }
+    
         return false;
     }
 }
