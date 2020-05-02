5cd7ad1c26c12e6244d9016933ba4a1a57b3c81f
dragonminja24 <michael.henrich422@gmail.com>
2020-05-02 17:11:20 +0200

    Added Test Cases

---
+++ A2.2/src/Sort.java
@@ -0,0 +1,11 @@
+public class Sort implements SortIF {
+    @Override
+    public void readDependencies(String[][] dependencies) {
+
+    }
+
+    @Override
+    public boolean isWellSorted(String[] sequence) {
+        return false;
+    }
+}
--- A2.2/src/SortIFTest.java
+++ A2.2/src/SortIFTest.java
@@ -1,16 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortIFTest {
+    String[][] dependencies;
+    String[] sequence1;
+    String[] sequence2;
+    Sort sort = new Sort();
 
-    @Test
-    void dependencies() {
-        fail("In Arbeit");
+    @BeforeEach
+    void setUp(){
+        dependencies = new String[][]{{"A", "B"}};
+        sequence1 = new String[]{"A","B"};
+        sequence2 = new String[]{"B","A"};
     }
 
     @Test
-    void isWellSorted() {
-        fail("In Arbeit");
+    @DisplayName("Test, bei dem eine richtige Sequenz als richtig erkannt werden soll")
+    void test1_isWellSorted() {
+        sort.readDependencies(dependencies);
+        boolean result1 = sort.isWellSorted(sequence1);
+        assertTrue(result1);
     }
+
+    @Test
+    @DisplayName("Test, bei dem eine falsche Sequenz als falsch erkannt werden soll")
+    void test2_isWellSorted() {
+        sort.readDependencies(dependencies);
+        boolean result2 = sort.isWellSorted(sequence2);
+        assertFalse(result2);
+    }
+
+
 }
