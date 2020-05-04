7eff90fe78e06af86771a0640c96d4a9865a3482
DanyalGh <60552630+DanyalGh@users.noreply.github.com>
2020-05-04 19:10:19 +0200

    transitive relations add
    still few bugfixes necessary

--- A2.2/src/Sort.java
+++ A2.2/src/Sort.java
@@ -1,7 +1,10 @@
+import javax.swing.*;
+import java.util.Arrays;
+
 public class Sort implements SortIF {
-    
+
     String[][] dependencies;
-    
+
     @Override
     public void readDependencies(String[][] dependencies) {
         this.dependencies = dependencies;
@@ -13,20 +16,45 @@ public class Sort implements SortIF {
             throw new IndexOutOfBoundsException();
         }
 
+        /*
         int i = 0;
+            for (String task : sequence) {
+                for (String[] dependency : dependencies) {
+                    if (dependency[1].equals(task)) {
+                        for (int j = i; j < sequence.length; j++) {
+                            if (dependency[0].equals(sequence[j])) {
+                                return false;
+                            }//end if
+                        }// end for
+                    }// end if
+                }//end for
+                i++;
+            }//end for
+        return true;
+*/
+
+
+        int i = 0;
+
         for (String task : sequence) {
             for (String[] dependency : dependencies) {
                 if (dependency[1].equals(task)) {
                     for (int j = i; j < sequence.length; j++) {
                         if (dependency[0].equals(sequence[j])) {
                             return false;
-                        }
-                    }
-                }
-            }
+                        }//end if
+                    }// end for
+                    for (String[] transitive: dependencies) {
+                        for (String task_ : sequence){
+                            if (transitive[1].equals(dependency[0]) && transitive[0].equals(task_) && !Arrays.asList(sequence).contains(dependency[0])){
+                                return false;
+                            }//end if
+                        }//end for
+                    }//end for
+                }// end if
+            }//end for
             i++;
-        }
-    
+        }//end for
         return true;
     }
 }
